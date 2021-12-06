6f4be87a2dce5b9a6e4fc08896d212ed9be6c6e6
Vlad J <vladjdk@gmail.com>
2021-12-06 00:02:19 -0500

    Staking boilerplate (#5)
    * receive_cw721
    
    * clippy
    
    * Remove queries
    
    * Removed States
    
    * fmt

--- contracts/internnft-staking-contract/src/error.rs
+++ contracts/internnft-staking-contract/src/error.rs
@@ -8,6 +8,9 @@ pub enum ContractError {
 
     #[error("Unauthorized")]
     Unauthorized {},
+
+    #[error("Invalid CW721 Receive Message")]
+    InvalidCw721ReceiveMsg {},
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
--- contracts/internnft-staking-contract/src/msg.rs
+++ contracts/internnft-staking-contract/src/msg.rs
@@ -1,3 +1,4 @@
+use cw721::Cw721ReceiveMsg;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -9,19 +10,17 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Receive(Cw721ReceiveMsg),
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
-}
+pub enum QueryMsg {}
 
-// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+#[serde(rename_all = "snake_case")]
+pub enum Cw721HookMsg {
+    // GetCount returns the current count as a json-encoded number
+    StakeGold {},
+    StakeExp {},
 }
